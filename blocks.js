55aecbf872ec6cb812e5b7e22ae2a87b5222d1f1
lukec11 <me@lukec.me>
2020-07-22 21:05:50 -0400

    initial (unfinished) commit

---
+++ blocks.js
@@ -0,0 +1,163 @@
+const requestArchive = (channel) => {
+    return {
+        title: {
+            type: 'plain_text',
+            text: 'Archivist',
+            emoji: true,
+        },
+        type: 'modal',
+        close: {
+            type: 'plain_text',
+            text: 'Close',
+            emoji: true,
+        },
+        blocks: [
+            {
+                type: 'section',
+                text: {
+                    type: 'mrkdwn',
+                    text:
+                        "Sorry, we don't support requesting archives right now :disappointed: Check back later for more info.",
+                },
+            },
+            {
+                type: 'context',
+                elements: [
+                    {
+                        type: 'plain_text',
+                        text:
+                            'Note: Channels will still be auto-archived after 6 months of inactivity!',
+                        emoji: true,
+                    },
+                ],
+            },
+            {
+                type: 'divider',
+            },
+            {
+                type: 'section',
+                text: {
+                    type: 'mrkdwn',
+                    text:
+                        'In the meantime, if you need a channel archived, head over to <#C0C78SG9L|hq> or message someone in <!subteam^S0DJXPY14|staff>.',
+                },
+            },
+        ],
+    }
+}
+const requestUnarchive = () => {
+    return {
+        title: {
+            type: 'plain_text',
+            text: 'Unarchive channel',
+            emoji: true,
+        },
+        submit: {
+            type: 'plain_text',
+            text: 'Submit',
+            emoji: true,
+        },
+        type: 'modal',
+        close: {
+            type: 'plain_text',
+            text: 'Cancel',
+            emoji: true,
+        },
+        blocks: [
+            {
+                type: 'section',
+                text: {
+                    type: 'mrkdwn',
+                    text:
+                        'Awesome! What channel would you like to :unlock: unarchive?',
+                },
+            },
+            {
+                type: 'divider',
+            },
+            {
+                type: 'input',
+                block_id: 'unarchive_channel_input_block',
+                element: {
+                    type: 'plain_text_input',
+                    action_id: 'unarchive_channel_input_action',
+                },
+                label: {
+                    type: 'plain_text',
+                    text: 'Select a channel (eg. #lounge)',
+                    emoji: true,
+                },
+            },
+        ],
+    }
+}
+const requestInit = () => {
+    return {
+        title: {
+            type: 'plain_text',
+            text: 'Archivist',
+            emoji: true,
+        },
+        type: 'modal',
+        close: {
+            type: 'plain_text',
+            text: 'Close',
+            emoji: true,
+        },
+        blocks: [
+            {
+                type: 'section',
+                text: {
+                    type: 'mrkdwn',
+                    text:
+                        "Hi there <@U012JBKFLUF>, I'm <@A017AQA46BF|Archivist>! I can help you out with all of your channel archiving needs.",
+                },
+            },
+            {
+                type: 'divider',
+            },
+            {
+                type: 'section',
+                text: {
+                    type: 'plain_text',
+                    text:
+                        'Would you like to request an archive or unarchive today?',
+                    emoji: true,
+                },
+            },
+            {
+                type: 'actions',
+                elements: [
+                    {
+                        type: 'button',
+                        text: {
+                            type: 'plain_text',
+                            text: 'Archive (WIP)',
+                            emoji: true,
+                        },
+                        value: 'request_archive',
+                        action_id: 'request_archive',
+                        style: 'danger',
+                    },
+                    {
+                        type: 'button',
+                        text: {
+                            type: 'plain_text',
+                            text: 'Unarchive',
+                            emoji: true,
+                        },
+                        value: 'request_unarchive',
+                        action_id: 'request_unarchive',
+                        style: 'primary',
+                    },
+                ],
+            },
+        ],
+    }
+}
+
+module.exports = {
+    requestInit,
+    requestUnarchive,
+    requestArchive,
+}
---
+++ index.js
@@ -0,0 +1,101 @@
+require('dotenv').config();
+
+const { WebClient } = require('@slack/web-api');
+const { createEventAdapter } = require('@slack/events-api');
+const { createMessageAdapter } = require('@slack/interactive-messages');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const wc = new WebClient(process.env.SLACK_OAUTH_TOKEN);
+const slackEvents = createEventAdapter(process.env.SLACK_SIGNING_SECRET);
+const slackInteractions = createMessageAdapter(
+    process.env.SLACK_SIGNING_SECRET
+);
+
+const app = express();
+
+app.use('/slack/events', slackEvents.expressMiddleware());
+app.use('/slack/interactions', slackInteractions.expressMiddleware());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//pull in blocks from blocks files
+const blocks = require('./blocks.js');
+const utils = require('./utils.js');
+
+app.post('/slack/interactions', async (req, res) => {
+    console.log(req.body);
+});
+
+app.post('/slack/archive-init', async (req, res) => {
+    //use body-parser to combat slack urlencoded weirdness
+    let { user_id, trigger_id } = req.body;
+
+    console.log(`User ${user} opened the modal with trigger ID ${trigger_id}`);
+    if (!utils.checkSigningSecret(req)) {
+        res.status(401).send('Unauthorized');
+        throw 'UnauthedAttempt';
+    }
+
+    //send an immediate 200 to slack so they don't complain
+    res.status(200).send('');
+
+    await wc.views.open({
+        token: process.env.SLACK_OAUTH_TOKEN,
+        trigger_id: trigger_id,
+        view: blocks.requestInit(),
+    });
+});
+
+slackInteractions.action({ actionId: 'request_archive' }, async (payload) => {
+    console.log(payload);
+    console.log('user requested archive of a channel, sending to new payload');
+    await wc.views.push({
+        trigger_id: payload.trigger_id,
+        view: blocks.requestArchive(),
+    });
+});
+
+slackInteractions.action({ actionId: 'request_unarchive' }, async (payload) => {
+    console.log('user requested a channel unarchive');
+
+    //unarchive channel & do things
+    await wc.views.push({
+        trigger_id: payload.trigger_id,
+        view: blocks.requestUnarchive(),
+    });
+});
+
+slackInteractions.viewSubmission(
+    { type: 'view_submission' }, //this is dumb and i know it's dumb, but it's required
+    async (payload, respond) => {
+        const block_id = await Object.keys(payload.view.state.values)[0]; //dumb code for dumb api
+
+        switch (block_id) {
+            case 'archive_channel_select_block': {
+                //do stuff - we have to figure out implemetation here
+                throw 'NotYetImplemented';
+            }
+            case 'unarchive_channel_input_block': {
+                const inputChannel = await payload.view.state.values[
+                    block_id
+                ].unarchive_channel_input_action.value.replace('#', '');
+                const channelId = await utils.getChannelId(inputChannel);
+                await utils.unarchiveChannel(await channelId); //TODO - check if channel exists before unarchiving
+                await utils.chat(
+                    channelId,
+                    `<@${payload.user.id}> unarchived this channel! Welcome back, everyone :wave:`
+                );
+                return {
+                    response_action: 'clear', //after unarchiving it will close the modal
+                };
+            }
+            default: {
+                return { response_action: 'clear' }; //if the user does nothing it will close the modal
+            }
+        }
+    }
+);
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('listening');
+});
