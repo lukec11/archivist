e6e96694a7247bd90bec0fd2749c6a2378a4216d
lukec11 <me@lukec.me>
2020-07-26 14:14:29 -0400

    use hack club's prettier

--- index.js
+++ index.js
@@ -1,117 +1,115 @@
-require('dotenv').config();
+require('dotenv').config()
 
-const { WebClient } = require('@slack/web-api');
-const { createEventAdapter } = require('@slack/events-api');
-const { createMessageAdapter } = require('@slack/interactive-messages');
-const express = require('express');
-const bodyParser = require('body-parser');
+const { WebClient } = require('@slack/web-api')
+const { createEventAdapter } = require('@slack/events-api')
+const { createMessageAdapter } = require('@slack/interactive-messages')
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const wc = new WebClient(process.env.SLACK_OAUTH_TOKEN);
-const slackEvents = createEventAdapter(process.env.SLACK_SIGNING_SECRET);
-const slackInteractions = createMessageAdapter(
-    process.env.SLACK_SIGNING_SECRET
-);
+const wc = new WebClient(process.env.SLACK_OAUTH_TOKEN)
+const slackEvents = createEventAdapter(process.env.SLACK_SIGNING_SECRET)
+const slackInteractions = createMessageAdapter(process.env.SLACK_SIGNING_SECRET)
 
-const app = express();
+const app = express()
 
-app.use('/slack/events', slackEvents.expressMiddleware());
-app.use('/slack/interactions', slackInteractions.expressMiddleware());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/slack/events', slackEvents.expressMiddleware())
+app.use('/slack/interactions', slackInteractions.expressMiddleware())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 //pull in blocks from blocks files
-const blocks = require('./blocks.js');
-const utils = require('./utils.js');
+const blocks = require('./blocks.js')
+const utils = require('./utils.js')
 
 //regexes
-const checkChannelsRegex = /checkOldChannels/;
-const checkOnlineRegex = /thump\sthump/;
+const checkChannelsRegex = /checkOldChannels/
+const checkOnlineRegex = /thump\sthump/
 
 app.post('/slack/archive-init', async (req, res) => {
-    //use body-parser to combat slack urlencoded weirdness
-    let { user_id, trigger_id } = req.body;
+  //use body-parser to combat slack urlencoded weirdness
+  let { user_id, trigger_id } = req.body
 
-    console.log(`User ${user} opened the modal with trigger ID ${trigger_id}`);
-    if (!utils.checkSigningSecret(req)) {
-        res.status(401).send('Unauthorized');
-        throw 'UnauthedAttempt';
+  console.log(`User ${user} opened the modal with trigger ID ${trigger_id}`)
+  if (!utils.checkSigningSecret(req)) {
+    res.status(401).send('Unauthorized')
+    throw 'UnauthedAttempt'
+  }
+
+  //send an immediate 200 to slack so they don't complain
+  res.status(200).send('')
+
+  await wc.views.open({
+    token: process.env.SLACK_OAUTH_TOKEN,
+    trigger_id: trigger_id,
+    view: blocks.requestInit()
+  })
+})
+
+slackEvents.on('message', async event => {
+  if (event.text.match(checkOnlineRegex)) {
+    await react(event.channel, 'heavy_check_mark', event.ts)
+  } else if (event.text.match(checkChannelsRegex)) {
+    console.log('Checking for outdated channels!')
+    const outdatedChannels = utils.getOldChannels
+    for (i of outdatedChannels) {
+      utils.renameDeadChannel(i)
+      chat(
+        process.env.SLACK_ADMIN_CHANNEL,
+        `Archived <#${i}> due to inactivity`
+      )
     }
+  }
+})
 
-    //send an immediate 200 to slack so they don't complain
-    res.status(200).send('');
+slackInteractions.action({ actionId: 'request_archive' }, async payload => {
+  console.log(payload)
+  console.log('user requested archive of a channel, sending to new payload')
+  await wc.views.push({
+    trigger_id: payload.trigger_id,
+    view: blocks.requestArchive()
+  })
+})
 
-    await wc.views.open({
-        token: process.env.SLACK_OAUTH_TOKEN,
-        trigger_id: trigger_id,
-        view: blocks.requestInit(),
-    });
-});
+slackInteractions.action({ actionId: 'request_unarchive' }, async payload => {
+  console.log('user requested a channel unarchive')
 
-slackEvents.on('message', async (event) => {
-    if (event.text.match(checkOnlineRegex)) {
-        await react(event.channel, 'heavy_check_mark', event.ts);
-    } else if (event.text.match(checkChannelsRegex)) {
-        console.log('Checking for outdated channels!');
-        const outdatedChannels = utils.getOldChannels;
-        for (i of outdatedChannels) {
-            utils.renameDeadChannel(i);
-            chat(
-                process.env.SLACK_ADMIN_CHANNEL,
-                `Archived <#${i}> due to inactivity`
-            );
-        }
-    }
-});
-
-slackInteractions.action({ actionId: 'request_archive' }, async (payload) => {
-    console.log(payload);
-    console.log('user requested archive of a channel, sending to new payload');
-    await wc.views.push({
-        trigger_id: payload.trigger_id,
-        view: blocks.requestArchive(),
-    });
-});
-
-slackInteractions.action({ actionId: 'request_unarchive' }, async (payload) => {
-    console.log('user requested a channel unarchive');
-
-    //unarchive channel & do things
-    await wc.views.push({
-        trigger_id: payload.trigger_id,
-        view: blocks.requestUnarchive(),
-    });
-});
+  //unarchive channel & do things
+  await wc.views.push({
+    trigger_id: payload.trigger_id,
+    view: blocks.requestUnarchive()
+  })
+})
 
 slackInteractions.viewSubmission(
-    { type: 'view_submission' }, //this is dumb and i know it's dumb, but it's required
-    async (payload, respond) => {
-        const block_id = await Object.keys(payload.view.state.values)[0]; //dumb code for dumb api
+  { type: 'view_submission' }, //this is dumb and i know it's dumb, but it's required
+  async (payload, respond) => {
+    const block_id = await Object.keys(payload.view.state.values)[0] //dumb code for dumb api
 
-        switch (block_id) {
-            case 'archive_channel_select_block': {
-                //do stuff - we have to figure out implemetation here
-                throw 'NotYetImplemented';
-            }
-            case 'unarchive_channel_input_block': {
-                const inputChannel = await payload.view.state.values[
-                    block_id
-                ].unarchive_channel_input_action.value.replace('#', '');
-                const channelId = await utils.getChannelId(inputChannel);
-                await utils.unarchiveChannel(await channelId); //TODO - check if channel exists before unarchiving
-                await utils.chat(
-                    channelId,
-                    `<@${payload.user.id}> unarchived this channel! Welcome back, everyone :wave:`
-                );
-                return {
-                    response_action: 'clear', //after unarchiving it will close the modal
-                };
-            }
-            default: {
-                return { response_action: 'clear' }; //if the user does nothing it will close the modal
-            }
+    switch (block_id) {
+      case 'archive_channel_select_block': {
+        //do stuff - we have to figure out implemetation here
+        throw 'NotYetImplemented'
+      }
+      case 'unarchive_channel_input_block': {
+        const inputChannel = await payload.view.state.values[
+          block_id
+        ].unarchive_channel_input_action.value.replace('#', '')
+        const channelId = await utils.getChannelId(inputChannel)
+        await utils.unarchiveChannel(await channelId) //TODO - check if channel exists before unarchiving
+        await utils.chat(
+          channelId,
+          `<@${payload.user.id}> unarchived this channel! Welcome back, everyone :wave:`
+        )
+        return {
+          response_action: 'clear' //after unarchiving it will close the modal
         }
+      }
+      default: {
+        return { response_action: 'clear' } //if the user does nothing it will close the modal
+      }
     }
-);
+  }
+)
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log('listening');
-});
+  console.log('listening')
+})
--- utils.js
+++ utils.js
@@ -1,108 +1,108 @@
-require('dotenv').config();
+require('dotenv').config()
 
-const { WebClient } = require('@slack/web-api');
-const fetch = require('node-fetch');
-const wc = new WebClient(process.env.SLACK_OAUTH_TOKEN);
+const { WebClient } = require('@slack/web-api')
+const fetch = require('node-fetch')
+const wc = new WebClient(process.env.SLACK_OAUTH_TOKEN)
 
 //to verify secrets from slack
-const crypto = require('crypto');
-const tsscmp = require('tsscmp');
-const { verifyRequestSignature } = require('@slack/events-api');
+const crypto = require('crypto')
+const tsscmp = require('tsscmp')
+const { verifyRequestSignature } = require('@slack/events-api')
 
-const blocks = require('./blocks.js');
+const blocks = require('./blocks.js')
 
 /**
  * Verifies whether or not a request from slack is legitimate
  * @param {Object} req | the express request object
  */
-const checkSigningSecret = (req) => {
-    const reqSignature = req.headers['x-slack-signature'];
-    const reqTs = req.headers['x-slack-request-timestamp'];
+const checkSigningSecret = req => {
+  const reqSignature = req.headers['x-slack-signature']
+  const reqTs = req.headers['x-slack-request-timestamp']
 
-    //create HMAC
-    const hmac = crypto.createHmac('sha256', process.env.SLACK_SIGNING_SECRET);
+  //create HMAC
+  const hmac = crypto.createHmac('sha256', process.env.SLACK_SIGNING_SECRET)
 
-    //update HMAC from slack
-    const [version, hash] = reqSignature.split('=');
-    hmac.update(`${version}:${reqTs}:${JSON.stringify(req.body)}`);
+  //update HMAC from slack
+  const [version, hash] = reqSignature.split('=')
+  hmac.update(`${version}:${reqTs}:${JSON.stringify(req.body)}`)
 
-    //returns boolean value of true or false
-    return tsscmp(hash, hmac.digest('hex'));
-};
+  //returns boolean value of true or false
+  return tsscmp(hash, hmac.digest('hex'))
+}
 
-const unarchiveChannel = async (channelId) => {
-    return await wc.conversations.unarchive({
-        token: process.env.SLACK_XOXS_TOKEN,
-        channel: channelId,
-    });
-};
+const unarchiveChannel = async channelId => {
+  return await wc.conversations.unarchive({
+    token: process.env.SLACK_XOXS_TOKEN,
+    channel: channelId
+  })
+}
 
 const postChannelPoll = async (channelId, user) => {
-    //do stuff
-};
+  //do stuff
+}
 
 const renameChannel = async (channelId, newName) => {
-    return await wc.conversations.rename({
-        channel: channelId,
-        name: newName,
-    });
-};
+  return await wc.conversations.rename({
+    channel: channelId,
+    name: newName
+  })
+}
 
-const getChannelId = async (channelName) => {
-    const res = await fetch(
-        `https://edgeapi.slack.com/cache/${process.env.SLACK_TEAM_ID}/channels/search`,
-        {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                token: process.env.SLACK_XOXS_TOKEN,
-                query: channelName,
-                count: 1,
-                filter: 'archived',
-            }),
-        }
-    );
-    const response = await res.json();
-    if (!(await response.results[0].name) === channelName) {
-        throw 'NotSameChannelError';
+const getChannelId = async channelName => {
+  const res = await fetch(
+    `https://edgeapi.slack.com/cache/${process.env.SLACK_TEAM_ID}/channels/search`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        token: process.env.SLACK_XOXS_TOKEN,
+        query: channelName,
+        count: 1,
+        filter: 'archived'
+      })
     }
-    if (!(await response.results.length) === 0) {
-        return null;
-    }
-    return await response.results[0].id;
-};
+  )
+  const response = await res.json()
+  if (!(await response.results[0].name) === channelName) {
+    throw 'NotSameChannelError'
+  }
+  if (!(await response.results.length) === 0) {
+    return null
+  }
+  return await response.results[0].id
+}
 
 const chat = async (channelId, text) => {
-    return await wc.chat.postMessage({
-        token: process.env.SLACK_OAUTH_TOKEN,
-        channel: channelId,
-        text: text,
-    });
-};
+  return await wc.chat.postMessage({
+    token: process.env.SLACK_OAUTH_TOKEN,
+    channel: channelId,
+    text: text
+  })
+}
 
 const react = async (channelId, reaction, ts) => {
-    return await wc.reactions.add({
-        token: process.env.SLACK_OAUTH_TOKEN,
-        channel: channelId,
-        timestamp: ts,
-        reaction: reaction,
-    });
-};
+  return await wc.reactions.add({
+    token: process.env.SLACK_OAUTH_TOKEN,
+    channel: channelId,
+    timestamp: ts,
+    reaction: reaction
+  })
+}
 
-const joinChannel = async (channelId) => {
-    await wc.conversations.join({
-        channel: channelId,
-    });
-};
+const joinChannel = async channelId => {
+  await wc.conversations.join({
+    channel: channelId
+  })
+}
 
 /**
  * @param {Date} date | a JavaScript "date" object
  */
-const getSeconds = (date) => {
-    return date.getTime() / 1000;
-};
+const getSeconds = date => {
+  return date.getTime() / 1000
+}
 
 /**
  * Gets a list of all channels without activity in the last 6 months
@@ -110,114 +110,114 @@ const getSeconds = (date) => {
  * (this also means it can't run serverless, as those shut down quickly after returning a response)
  * @param {int} age - the time in seconds that the channel should be older than to be considered "old"
  */
-const getOldChannels = async (age) => {
-    let deadChannels = [];
-    let currentDate = getSeconds(new Date());
+const getOldChannels = async age => {
+  let deadChannels = []
+  let currentDate = getSeconds(new Date())
 
-    try {
-        let channels = await wc.conversations.list({
-            token: process.env.SLACK_OAUTH_TOKEN,
-            exclude_archived: true, //don't show already-archived channels
-            limit: 1000, //1000 is max count of channels - can get around this if necessary but it's annoying so not going to bother yet
-        });
+  try {
+    let channels = await wc.conversations.list({
+      token: process.env.SLACK_OAUTH_TOKEN,
+      exclude_archived: true, //don't show already-archived channels
+      limit: 1000 //1000 is max count of channels - can get around this if necessary but it's annoying so not going to bother yet
+    })
 
-        channels = channels.channels;
+    channels = channels.channels
 
-        for (let channel of channels) {
-            if (!channel.is_member) {
-                await joinChannel(channel.id);
-            }
-            const lastMessage = await wc.conversations.history({
-                token: process.env.SLACK_OAUTH_TOKEN, //NOTE: This is a tier 3 rate limit. Make sure it doesn't execed or there is a catch in place
-                channel: channel.id,
-                limit: 1, //limits to only the last sent message
-            });
-            if (!(await lastMessage.ok)) {
-                // check if it wasn't able to successfully get the message
-                await chat(
-                    process.env.SLACK_ADMIN_CHANNEL,
-                    `Experienced an error when getting messages for <#${channel.id}>!`
-                );
-                console.log(`Failed to get messages for #${channel.name}!`);
-                continue;
-            }
-            if ((await lastMessage.messages.length) === 0) {
-                console.log(
-                    `Couldn't calculate age of #${channel.name} (no messages), ignoring!`
-                );
-                continue;
-            }
-            const lastMessageTs = await lastMessage.messages[0].ts; //gets the unix timestamp of the last message, in seconds
-
-            if (
-                currentDate - (await lastMessageTs) >
-                age // &&
-                //				!clubChannels.includes(channel.id)
-            ) {
-                console.log(`Dead channel found: ${channel.id}!`);
-                deadChannels.push(channel.id);
-            }
-        }
-
-        return await deadChannels;
-    } catch (err) {
-        console.log(`Error while looking for dead channels! ${err}`);
+    for (let channel of channels) {
+      if (!channel.is_member) {
+        await joinChannel(channel.id)
+      }
+      const lastMessage = await wc.conversations.history({
+        token: process.env.SLACK_OAUTH_TOKEN, //NOTE: This is a tier 3 rate limit. Make sure it doesn't execed or there is a catch in place
+        channel: channel.id,
+        limit: 1 //limits to only the last sent message
+      })
+      if (!(await lastMessage.ok)) {
+        // check if it wasn't able to successfully get the message
         await chat(
-            process.env.SLACK_ADMIN_CHANNEL,
-            `Error while looking for dead channels! Trace: \`\`\`${err}\`\`\``
-        );
+          process.env.SLACK_ADMIN_CHANNEL,
+          `Experienced an error when getting messages for <#${channel.id}>!`
+        )
+        console.log(`Failed to get messages for #${channel.name}!`)
+        continue
+      }
+      if ((await lastMessage.messages.length) === 0) {
+        console.log(
+          `Couldn't calculate age of #${channel.name} (no messages), ignoring!`
+        )
+        continue
+      }
+      const lastMessageTs = await lastMessage.messages[0].ts //gets the unix timestamp of the last message, in seconds
+
+      if (
+        currentDate - (await lastMessageTs) >
+        age // &&
+        //				!clubChannels.includes(channel.id)
+      ) {
+        console.log(`Dead channel found: ${channel.id}!`)
+        deadChannels.push(channel.id)
+      }
     }
-};
 
-const renameDeadChannel = async (channelId) => {
-    try {
-        const channelInfo = await wc.conversations.info({
-            token: process.env.SLACK_OAUTH_TOKEN,
-            channel: channelId,
-        });
+    return await deadChannels
+  } catch (err) {
+    console.log(`Error while looking for dead channels! ${err}`)
+    await chat(
+      process.env.SLACK_ADMIN_CHANNEL,
+      `Error while looking for dead channels! Trace: \`\`\`${err}\`\`\``
+    )
+  }
+}
 
-        if (!(await channelInfo.ok)) {
-            //check for an error in getting channel info
-            chat(
-                process.env.SLACK_ADMIN_CHANNEL,
-                `Failed to get info for rename on <#${channelId}> :(`
-            );
-            console.log(`Couldn't get info on ${channelId}!`);
-            throw 'channelInfoNotGathered';
-        }
+const renameDeadChannel = async channelId => {
+  try {
+    const channelInfo = await wc.conversations.info({
+      token: process.env.SLACK_OAUTH_TOKEN,
+      channel: channelId
+    })
 
-        const chnanelName = await channelInfo.channel.name;
-        if ((await getChannelId(`zzz-${channelName}`)) != null) {
-            //this means the zzz- channel already exists! that's bad.
-            console.log(
-                `An archived channel with this name already exists! We can't archive it.`
-            );
-            chat(
-                process.env.SLACK_ADMIN_CHANNEL,
-                `An archived copy of <#${channelId}> already exists, we couldn't archive it.`
-            );
-            throw 'ArchiveAlreadyExists';
-        }
-
-        renameChannel(channelId, `zzz-${channelName}`);
-    } catch (err) {
-        console.log(`Failed to rename dead channel ${channelId}!`);
-        chat(
-            process.env.SLACK_ADMIN_CHANNEL,
-            `Failed to rename dead channel <#${channelId}>!`
-        );
+    if (!(await channelInfo.ok)) {
+      //check for an error in getting channel info
+      chat(
+        process.env.SLACK_ADMIN_CHANNEL,
+        `Failed to get info for rename on <#${channelId}> :(`
+      )
+      console.log(`Couldn't get info on ${channelId}!`)
+      throw 'channelInfoNotGathered'
     }
-};
+
+    const chnanelName = await channelInfo.channel.name
+    if ((await getChannelId(`zzz-${channelName}`)) != null) {
+      //this means the zzz- channel already exists! that's bad.
+      console.log(
+        `An archived channel with this name already exists! We can't archive it.`
+      )
+      chat(
+        process.env.SLACK_ADMIN_CHANNEL,
+        `An archived copy of <#${channelId}> already exists, we couldn't archive it.`
+      )
+      throw 'ArchiveAlreadyExists'
+    }
+
+    renameChannel(channelId, `zzz-${channelName}`)
+  } catch (err) {
+    console.log(`Failed to rename dead channel ${channelId}!`)
+    chat(
+      process.env.SLACK_ADMIN_CHANNEL,
+      `Failed to rename dead channel <#${channelId}>!`
+    )
+  }
+}
 
 module.exports = {
-    checkSigningSecret,
-    unarchiveChannel,
-    postChannelPoll,
-    renameChannel,
-    getChannelId,
-    chat,
-    react,
-    joinChannel,
-    getOldChannels,
-    renameDeadChannel,
-};
+  checkSigningSecret,
+  unarchiveChannel,
+  postChannelPoll,
+  renameChannel,
+  getChannelId,
+  chat,
+  react,
+  joinChannel,
+  getOldChannels,
+  renameDeadChannel
+}
