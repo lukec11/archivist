71b77a318ab25ed44285817202457f118747ea8b
lukec11 <me@lukec.me>
2020-07-28 20:19:51 -0400

    add stuff

--- index.js
+++ index.js
@@ -24,19 +24,26 @@ const utils = require('./utils.js')
 const checkChannelsRegex = /checkOldChannels/
 const checkOnlineRegex = /checkIfOnline/
 
+/**
+ * Listens for POST from slack slash command
+ */
 app.post('/slack/archive-init', async (req, res) => {
-  //use body-parser to combat slack urlencoded weirdness
+  //use body-parser to grab userid and triggerid
   let { user_id, trigger_id } = req.body
 
-  console.log(`User ${user} opened the modal with trigger ID ${trigger_id}`)
+  console.log(
+    `User ${user} opened the slash command modal with trigger ID ${trigger_id}`
+  )
   if (!utils.checkSigningSecret(req)) {
+    //checks request to see that the HMAC is correct
     res.status(401).send('Unauthorized')
     throw 'UnauthedAttempt'
   }
 
-  //send an immediate 200 to slack so they don't complain
-  res.status(200).send('')
+  //send an immediate 200 to slack so they don't complain about timeouts
+  await res.status(200).send('')
 
+  //opens a modal with the requestInit blocks - asking if they want to archive or unarchive
   await wc.views.open({
     token: process.env.SLACK_OAUTH_TOKEN,
     trigger_id: trigger_id,
@@ -44,22 +51,28 @@ app.post('/slack/archive-init', async (req, res) => {
   })
 })
 
+/**
+ * Detects on any new slack message that it can see
+ */
 slackEvents.on('message', async event => {
   if (event.text.match(checkOnlineRegex)) {
-    await react(event.channel, 'heavy_check_mark', event.ts)
+    await react(event.channel, 'heavy_check_mark', event.ts) //react with a thumbs up to confirm that the bot's online and alive
   } else if (event.text.match(checkChannelsRegex)) {
     console.log('Checking for outdated channels!')
     const outdatedChannels = utils.getOldChannels
     for (i of outdatedChannels) {
-      utils.renameDeadChannel(i)
+      utils.renameDeadChannel(i) //renames channel with zzz-
       chat(
         process.env.SLACK_ADMIN_CHANNEL,
         `Archived <#${i}> due to inactivity`
-      )
+      ) //exlains the archive
     }
   }
 })
 
+/*
+ * If the user requests an archive in the init menu
+ */
 slackInteractions.action({ actionId: 'request_archive' }, async payload => {
   console.log(payload)
   console.log('user requested archive of a channel, sending to new payload')
@@ -69,30 +82,36 @@ slackInteractions.action({ actionId: 'request_archive' }, async payload => {
   })
 })
 
+/*
+ * If the user requests an unarchive in the init menu
+ */
 slackInteractions.action({ actionId: 'request_unarchive' }, async payload => {
   console.log('user requested a channel unarchive')
 
-  //unarchive channel & do things
+  //push unarchive payload
   await wc.views.push({
     trigger_id: payload.trigger_id,
     view: blocks.requestUnarchive()
   })
 })
 
+/*
+ * Checks for a view/submission of multiple types - currently just unarchive but will be more later
+ */
 slackInteractions.viewSubmission(
-  { type: 'view_submission' }, //this is dumb and i know it's dumb, but it's required
+  { type: 'view_submission' },
   async (payload, respond) => {
     const block_id = await Object.keys(payload.view.state.values)[0] //dumb code for dumb api
 
     switch (block_id) {
       case 'archive_channel_select_block': {
-        //do stuff - we have to figure out implemetation here
+        //do stuff - we have to figure out implemetation here before we can write the code
         throw 'NotYetImplemented'
       }
       case 'unarchive_channel_input_block': {
         const inputChannel = await payload.view.state.values[
           block_id
-        ].unarchive_channel_input_action.value.replace('#', '')
+        ].unarchive_channel_input_action.value.replace('#', '') //removes "#" from the channel selector, if people put it in
         const channelId = await utils.getChannelId(inputChannel)
         await utils.unarchiveChannel(await channelId) //TODO - check if channel exists before unarchiving
         await utils.chat(
@@ -104,7 +123,7 @@ slackInteractions.viewSubmission(
         }
       }
       default: {
-        return { response_action: 'clear' } //if the user does nothing it will close the modal
+        return { response_action: 'clear' } //if the user submits with nothing in, the modal closes
       }
     }
   }
